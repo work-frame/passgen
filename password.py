c359c127cc0c70ab8d6f08031328ea413a931500
= <=>
2023-11-22 08:14:38 +0100

    first commit

---
+++ password.py
@@ -0,0 +1,19 @@
+import random 
+
+print('Welcome to your Password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&^&*()_+;".0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nhere are your passwords:')
+
+for pwd in range (number):
+          passwords = ''
+          for c in range(length):
+                  passwords += random.choice(chars)
+                  print(passwords)
